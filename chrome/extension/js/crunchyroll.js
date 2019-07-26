104e52baa4d54471ade25f9b29449ff9baf7f5c1
Thomas TAVERNIER <thomas@tavernier.dev>
2019-07-26 16:40:29 +0200

    2.0.1
    - clear code, inject css
    - add comments

--- chrome/extension/js/crunchyroll.js
+++ chrome/extension/js/crunchyroll.js
@@ -1,112 +1,47 @@
-var ratio = window.screen.availHeight / window.screen.availWidth;
+// This code is to be inserted in "https://www.crunchyroll.com/*"
+// If we are on a video page, it will make the video player bigger
 
-// refresh function
-function refresh() {
-	window.dispatchEvent(new Event("resize"));
-}
-
-// remove Ads
-function removeAds() {
-	var styleSheetRemoveAds = document.createElement('style');
-	styleSheetRemoveAds.innerHTML = `
-		#message_box {
-			display: none !important;
-		}
-		.cr-expo-banner {
-			display: none !important;
-		}
-		body {
-			background-image: none !important;
-			background: none !important;
-		}
-		#template_skin_leaderboard {
-			height: 0 !important;
-		}
-		#template_skin_splashlink {
-			pointer-events: none !important;
-		}
-		.__web-inspector-hide-shortcut__ {
-			pointer-events: none !important;
-		}
-	`
-	document.head.appendChild(styleSheetRemoveAds);
-}
-
-// set style as bigger player function
+// set style as bigger player
 function setStyleAsBiggerPlayer() {
-	var style = document.createElement('style');
-	document.head.appendChild(style);
-	if (document.getElementById("main_content") !== null) {
+	// if we are on a video page
+	if (document.getElementById("showmedia_video_player") !== null) {
+		// if user isn't log in
 		if (document.getElementById("showmedia_video_box") !== null) {
 			document.getElementsByClassName("showmedia-trail cf")[0].appendChild(document.getElementById("showmedia_video"));
 			document.getElementById("showmedia_video_box").id = "showmedia_video_box_wide";
 		}
+		// create a style sheet
+		var style = document.createElement('style');
+		// write style sheet
 		style.innerHTML = `
-		#showmedia, #footer {
-			width: 960px !important;
-			margin: 0 auto !important;
-		}
 		#showmedia_video {
-			height: calc((` + ratio + `) * 100vw);;
-			background-color: black !important;
-			width: 100% !important;
-			min-width: 960px !important;
-			min-height: 540px !important;
-			max-height: 100vh !important;
-		}
-		#showmedia_video_box_wide {
-			width: 100% !important;
-			height: 100% !important;
-		}
-		#template_scroller {
-			padding-top: 0px !important;
+			height: calc((` + window.screen.availHeight / window.screen.availWidth + `) * 100vw);;
 		}
 		#template_container {
-			padding: 0px !important;
+			padding: 0 !important;
 			width: 100% !important;
-		}
+		}	
 		#header_beta {
-			opacity: 0;
-			position: absolute;
-			z-index: 3;
-			width: 100%;
-			transition: opacity 0.8s;
-		}
-		#header_beta:hover {
-			opacity: 1;
-			transition: opacity 0.4s;
-		}
-		.showmedia-header:first-child {
-			display: none !important;
-		}
-		#vilos-player {
-			width: calc(100% + 3px) !important;
-			height: calc(100% + 1px) !important;
-			margin-left: -3px !important;
-			margin-top: -1px !important;
-		}
-		#showmedia_video_player {
-			overflow: hidden !important;
+    		opacity: 0;
+    		position: absolute;
 		}
 		`
+		// insert style sheet
+		document.head.appendChild(style);
 	}
-	// .html5-video-player {
-	// 	width: 100% !important;
-	// 	height: 100% !important;
-	// }
 }
 
 // set focus without crunchyrollHTML5 function
 function setFocus() {
+	// if we are on a video page
 	if (document.getElementById("showmedia_video_player") !== null) {
+		//set focus to the video
 		document.getElementById("showmedia_video_player").firstChild.focus();
 	}
 }
 
 // init
 function init() {
-	// remove ads
-	setTimeout(removeAds);
 	// set big player
 	setTimeout(setStyleAsBiggerPlayer);
 	// set focus to the video player
--- chrome/extension/js/crunchyrollVideoPlayer.js
+++ chrome/extension/js/crunchyrollVideoPlayer.js
@@ -1,14 +1,20 @@
+// This code is to be inserted in "https://static.crunchyroll.com/vilos/player.html", it's the iframe of "https://www.crunchyroll.com/*" video player
+// It will create and insert buttons in the video player so that we can move forward or backward
+
+// back X seconds
 function back(evt) {
     var video = document.getElementById("player_html5_api");
     video.currentTime = video.currentTime - parseInt(evt.target.id);
 }
 
+// skip X seconds
 function skip(evt) {
     var video = document.getElementById("player_html5_api");
     video.currentTime = video.currentTime + parseInt(evt.target.id);
 }
 
-function insertDiv() {
+// insert buttonns
+function insertButtons() {
     var backList = [30, 10];
     var skipList = [30, 90];
     var buttonList = [];
@@ -16,7 +22,7 @@ function insertDiv() {
         var backButton = document.createElement("div");
         backButton.innerHTML = "«" + backNumber;
         backButton.id = backNumber;
-        backButton.title = "back " + backNumber +"s";
+        backButton.title = "back " + backNumber + "s";
         backButton.addEventListener("click", back);
         buttonList.push(backButton);
     }
@@ -25,7 +31,7 @@ function insertDiv() {
         var skipButton = document.createElement("div");
         skipButton.innerHTML = skipNumber + "»";
         skipButton.id = skipNumber;
-        skipButton.title = "skip " + skipNumber +"s";
+        skipButton.title = "skip " + skipNumber + "s";
         skipButton.addEventListener("click", skip);
         buttonList.push(skipButton);
     }
@@ -36,27 +42,9 @@ function insertDiv() {
     }
 }
 
-function insertStyle() {
-    var style = document.createElement("style");
-    style.innerHTML = `
-    .MIB_button {
-        padding: 0 5px !important;
-    }
-    .vjs-duration {
-        padding: 0 5px 0 0 !important;
-    }
-    .MIB_button:hover {
-        color: coral !important;
-        background-color: #131318;
-    }
-    `
-    document.head.appendChild(style);
-}
-
 function init() {
-    insertStyle();
-    insertDiv();
-    console.log(123);
+    // insert buttonns
+    setTimeout(insertButtons);
 }
 
 init();
