54fec3baeb86df95aa7c4e2a51cb5d169871c544
Thomas TAVERNIER <thomas@tavernier.dev>
2020-02-09 20:42:15 +0100

    2.5.2
    height ratio in css, no more in js

--- extension/js/crunchyroll.js
+++ extension/js/crunchyroll.js
@@ -1,17 +1,8 @@
 function init() {
 	if (document.getElementById('showmedia_video')) {
-		let style = document.createElement('style');
-		style.innerHTML = `
-    		html[cbp_video_page=true][cbp_scrollbar=true] #showmedia_video {
-				height: calc((${window.screen.height / window.screen.width}) * 100vw);
-    		}
-    		html[cbp_video_page=true][cbp_scrollbar=false] #showmedia_video {
-				height: calc((${window.screen.height / window.screen.width}) * (100vw - ${window.innerWidth - document.body.offsetWidth}px));
-    		}`;
-		document.head.appendChild(style);
 		document.documentElement.setAttribute('cbp_video_page', true);
 
-		// if user isn't log in
+		// if user isn't logged in
 		if (document.getElementById('showmedia_video_box') !== null) {
 			document.documentElement.setAttribute('cbp_not_logged', true);
 			document.querySelector('.showmedia-trail.cf').appendChild(document.getElementById('showmedia_video'));
