53c90e5e1130657c203bc580a09b28c97a3a4a9a
Thomas TAVERNIER <thomas@tavernier.dev>
2020-02-21 17:55:52 +0100

    2.6.0
    - add playback speed controls in player existing settings

--- extension/js/crunchyrollVideoPlayerV2.js
+++ extension/js/crunchyrollVideoPlayerV2.js
@@ -21,6 +21,11 @@ function parseNumber(number) {
     return numberMinutes > 0 ? `${numberMinutes}:${numberSeconds < 10 ? '0' + numberSeconds : numberSeconds}` : numberSeconds;
 }
 
+function translate(key) {
+    let label = chrome.i18n.getMessage(key);
+    return label !== '' ? label : key;
+}
+
 function createFastForwardBackwardButtons() {
     cbp_div_player_controls.innerHTML = '';
     let fastBackwardList = chromeStorage.fast_backward_buttons.split(',');
@@ -59,6 +64,7 @@ function createDivs() {
     [cbp_div_player_controls, cbp_div_player_mode].forEach(div => div.className = 'cbp_div css-1dbjc4n r-1awozwy r-18u37iz r-1pi2tsx');
 
     createPlayerButton();
+    createPlayerSettings();
     createFastForwardBackwardButtons();
 }
 
@@ -116,12 +122,140 @@ function createPlayerButton() {
     });
 }
 
+function createSettingsDiv(title, value, type) {
+    const div = document.createElement('div');
+    div.className = 'cbp_menu';
+    div.innerHTML = `
+        <div class="font">${title}</div>
+        <div class="right">
+          <div class="right_text">
+            <span class="font" id="cbp_${type}">
+              ${value}
+            </span>
+          </div>
+          <div class="next"></div>
+        </div>`;
+    div.addEventListener('click', () => document.getElementById('vilosSettingsMenu').setAttribute('cbp-options', type))
+    return div;
+}
+
+function createSettingsOptionsDiv(title, type, options, value, callback) {
+    const back = document.createElement('div');
+    back.className = `cbp_back cbp_${type}`;
+    back.innerHTML = `
+        <div class="back"></div>
+        <div class="font">${title}</div>`;
+    back.addEventListener('click', () => {
+        document.getElementById('vilosSettingsMenu').setAttribute('cbp-options', 'menu');
+    });
+    const submenu = document.createElement('div');
+    submenu.id = 'vilosRadioSubmenu';
+    submenu.className = `cbp_${type} cbp_options`;
+    options.forEach(option => {
+        const subtMenuItem = document.createElement('div');
+        subtMenuItem.className = 'cbp_option';
+        subtMenuItem.innerHTML = `
+            <svg viewBox="0 0 20 20" style="height: 20px; width: 20px;">
+            <circle
+              class="bg"
+              cx="10"
+              cy="10"
+              r="9"
+              style="fill: rgb(25, 46, 56); opacity: 1;"
+            ></circle>
+            <circle
+              class="dot"
+              cx="10"
+              cy="10"
+              r="4"
+              style="fill: rgb(68, 195, 171);"
+            ></circle>
+            <path
+              class="outer_circle"
+              d="M10,2a8,8,0,1,1-8,8,8.009,8.009,0,0,1,8-8m0-2A10,10,0,1,0,20,10,10,10,0,0,0,10,0Z"
+              style="fill: rgb(160, 160, 160);"
+            ></path>
+            </svg>
+            <div class="text">
+                <span class="font">${option.name ? translate(option.name) : option.value}</span>
+            </div>`;
+        subtMenuItem.setAttribute('value', option.value == value);
+        subtMenuItem.addEventListener('click', function () {
+            const selected = document.querySelector('.cbp_option[value=true]');
+            if (selected) selected.setAttribute('value', false);
+            this.setAttribute('value', true);
+            localStorage.setItem(`Vilos:${type}`, option.value);
+            document.getElementById(`cbp_${type}`).innerHTML = option.value;
+            callback(option.value);
+        });
+        submenu.appendChild(subtMenuItem);
+    })
+    return [back, submenu];
+}
+
+function createPlayerSettings() {
+    cbp_div_settings = [];
+    [{
+        title: 'KEY_PLAYBACK_RATE',
+        type: 'playbackRate',
+        defaultValue: 1,
+        values: [{
+            value: 0.25,
+        }, {
+            value: 0.5,
+        }, {
+            value: 0.75,
+        }, {
+            value: 1,
+            name: 'KEY_NORMAL',
+        }, {
+            value: 1.25,
+        }, {
+            value: 1.5,
+        }, {
+            value: 1.75,
+        }, {
+            value: 2,
+        }],
+        callback: (value) => document.getElementById('player0').playbackRate = value,
+    }].map(setting => {
+        let value = localStorage.getItem(`Vilos:${setting.type}`);
+        if (!value) value = setting.defaultValue;
+        const title = translate(setting.title);
+        let selectedValueName = setting.values.find(v => v.value == value);
+        selectedValueName = selectedValueName ? (selectedValueName.name ? translate(selectedValueName.name) : selectedValueName.value) : value;
+        setting.callback(value);
+        return [
+            createSettingsDiv(title, selectedValueName, setting.type),
+            ...createSettingsOptionsDiv(title, setting.type, setting.values, value, setting.callback)
+        ];
+    }).forEach(list => list.forEach(div => cbp_div_settings.push(div)));
+}
+
 function insertCbpDivs(vilosControlsContainer) {
     if (vilosControlsContainer.style.opacity !== '') {
         const overlay = vilosControlsContainer.children[0];
         const controlsBar = overlay.children[overlay.children[1] !== undefined ? 1 : 0].children[2];
         controlsBar.children[0].appendChild(cbp_div_player_controls);
         controlsBar.children[1].insertBefore(cbp_div_player_mode, controlsBar.children[1].children[1]);
+
+        new MutationObserver((mutationsList) => {
+                const vilosSettingsMenu = mutationsList[0].addedNodes[0];
+                if (vilosSettingsMenu) {
+                    const firstChild = vilosSettingsMenu.firstChild;
+                    vilosSettingsMenu.setAttribute('cbp-options', 'menu');
+                    cbp_div_settings.forEach(element => vilosSettingsMenu.insertBefore(element, firstChild));
+                    new MutationObserver(() => {
+                            vilosSettingsMenu.setAttribute('cbp-options', document.querySelector('[data-testid="vilos-settings_back_button"]') ? 'submenu' : 'menu')
+                        })
+                        .observe(vilosSettingsMenu, {
+                            childList: true,
+                        });
+                }
+            })
+            .observe(controlsBar.children[1].children[0], {
+                childList: true,
+            });
     }
 }
 
@@ -165,5 +299,6 @@ function init() {
 
 let cbp_div_player_controls;
 let cbp_div_player_mode;
+let cbp_div_settings;
 
 setTimeout(init);
