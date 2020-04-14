34e8850ac119609759e2e82dbc9307e9d6c800f3
Thomas TAVERNIER <thomas@tavernier.dev>
2020-04-14 16:52:02 +0200

    2.7.2
    Fix infinite loading when user isn't logged in

--- extension/js/crunchyroll.js
+++ extension/js/crunchyroll.js
@@ -1,9 +1,10 @@
 function init() {
   // if video page && user isn't logged in
   if (document.documentElement.getAttribute('ic_video_page') && document.getElementById('showmedia_video_box')) {
-    document.documentElement.setAttribute('ic_not_logged', true);
-    document.querySelector('.showmedia-trail.cf').appendChild(document.getElementById('showmedia_video'));
+    document.documentElement.setAttribute('ic_not_logged_in', true);
     document.getElementById('showmedia_video_box').id = 'showmedia_video_box_wide';
+    document.getElementById('showmedia_video').after(document.getElementById('showmedia_video').cloneNode(false));
+    document.getElementById('sidebar').prepend(document.getElementById('showmedia_video').cloneNode(false));
   }
 }
 
