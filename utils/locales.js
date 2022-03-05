9206dcc82fc25e6e6f4c11aa2382e87120e97621
Thomas TAVERNIER <thomas@tavernier.dev>
2022-03-05 13:58:17 +0100

    build: create locale script

---
+++ utils/locales.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const read = (locale) => require(`../_locales/${locale}/messages.json`);
+const write = (locale, data) =>
+  fs.writeFileSync(
+    `${__dirname}/../_locales/${locale}/messages.json`,
+    JSON.stringify(Object.fromEntries(Object.entries(data).sort(([a], [b]) => a.localeCompare(b))), null, 2),
+  );
+const en = read('en');
+write('en', en);
+['es', 'fr', 'pt_BR'].forEach((locale) => {
+  const data = read(locale);
+  write(
+    locale,
+    Object.fromEntries(Object.entries(en).map(([key, value]) => [key, data[key] || value])),
+  );
+});
