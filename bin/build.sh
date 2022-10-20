8721cb8dcf80059a9bdde342bdbf745d2576f6e3
Thomas TAVERNIER <thomas@tavernier.dev>
2022-10-20 17:54:55 +0200

    build: fix build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,2 +1,5 @@
-test -f ./$(git rev-parse --abbrev-ref HEAD)-*.zip && rm ./$(git rev-parse --abbrev-ref HEAD)-*.zip
-git archive --format zip --output "$(git rev-parse --abbrev-ref HEAD)-$(git show HEAD:manifest.json | grep '"version"' | cut -d\" -f4)".zip HEAD
+cd $(git rev-parse --show-toplevel)
+mkdir -p build
+output=./build/$(git rev-parse --abbrev-ref HEAD)-$(git show HEAD:manifest.json | grep '"version"' | cut -d\" -f4).zip
+test -f $output && rm $output
+git archive --format=zip --output=$output HEAD
